4d476147cf5a9161dc66025db3903b438653fc97
injashkin <injashkin@gmail.com>
2022-12-21 17:00:06 +0300

    Update colors.css.ts

--- src/colors.css.ts
+++ src/colors.css.ts
@@ -1,8 +1,8 @@
 export const colors = {
   background: "#fff",
-  text: "#004ca3",
-  primary: "#004ca3",
-  muted: "#f5fcff",
+  text: "#333", // "#004ca3"
+  primary: "#57d7b4", // "#004ca3"
+  muted: "#e5e5e5", // "#f5fcff"
   active: "#001d3d",
   black: "#000",
 }
